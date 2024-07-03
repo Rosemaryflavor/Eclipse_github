640157180a2499e77eca6f4d61a4a17d54370f2d
Rosemaryflavor <weesy44@naver.com>
2024-07-03 12:34:58 +0900

    0703헤이헤이헤이

--- java_git_test/src/java_05/Circle.java
+++ java_git_test/src/java_05/Circle.java
@@ -11,6 +11,26 @@ public class Circle {
 	public Circle(int x, int y) {
 		System.out.println("Circle 생성자 호출");
 	}
+
+	public String getX() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getY() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setX(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setY(int i) {
+		// TODO Auto-generated method stub
+		
+	}
 	}
 
 
